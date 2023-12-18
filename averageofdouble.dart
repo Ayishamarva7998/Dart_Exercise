554203bcfa9d1633edfefcc7a2efa5fb48ac9f21
Ayishamarva7998 <ayshaaysha7998@gmail.com>
2023-12-18 22:18:36 +0530

    more than a number

--- averageofdouble.dart
+++ averageofdouble.dart
@@ -1,5 +1,5 @@
 void main() {
-  List<double> doubleList = [2.5, 1.5, 3.0, 4.5];
+  List<double> doubleList = [7.3, 1.5, 3.0, 4.5];
 
   double sum = doubleList.reduce((value, element) => value + element);
   double average = sum / doubleList.length;
